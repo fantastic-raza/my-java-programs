e2582e372847608f10a9b836934a4890294e5597
ali <kraza10000@gmail.com>
2022-08-09 23:53:35 -0700

    adding all file for first time

---
+++ multiply.java
@@ -0,0 +1,14 @@
+class multiply
+{
+  public static void main (String[]args)
+  {
+    /* code */
+
+    int a = 2;
+    int b = 4;
+    int c = a * b;
+    int d = b - a;
+    int e = c / d;
+      System.out.print (e);
+  }
+}
